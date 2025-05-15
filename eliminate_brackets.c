5490e8b707fe824d0b9bb9d81e2d8a554017bda2
luca01github <luca.porcheddu01@gmail.com>
2025-05-16 00:59:49 +0200

    Add files via upload
    Eliminates strings between brackets, and rewrites the whole text on a clean file

---
+++ eliminate_brackets.c
@@ -0,0 +1,33 @@
+//Writes on the output file the cleaned version of the text, without sentences between brackets [] 
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(){
+    FILE *fPtr = fopen("romeo-and-juliet_TXT_FolgerShakespeare.txt", "r");
+    FILE *foutput = fopen("RomeoAndJuliet-cleaned.txt", "w");
+    
+    if(fPtr == NULL || foutput == NULL){
+        perror("Error while opening the file\n");
+        return 1;
+    }
+
+    char ch;
+    bool inside_br = false;
+
+    while((ch = fgetc(fPtr)) != EOF){
+        if(ch == '['){
+            inside_br = true;
+        }
+        else if(ch == ']'){
+            inside_br = false;
+        }
+        else if(!inside_br){
+            fputc(ch, foutput);
+        }
+    }
+    
+    fclose(fPtr);
+    fclose(foutput);
+}
